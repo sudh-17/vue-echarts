318830e5cf77c9228cc608a666a98bc7dc05681a
Chris Pan <chrispan.pt@gmail.com>
2016-03-02 19:03:30 +0800

    Add basic demos.

--- src/views/line/index.js
+++ src/views/line/index.js
@@ -1,3 +1,38 @@
 module.exports = {
-    template: require('./template.html')
+    template: require('./template.html'),
+    data: function () {
+        return {
+            lineChartOption: {}
+        }
+    },
+    created: function () {
+        var vm = this;
+
+        vm.lineChartOption = {
+            tooltip : {
+                trigger: 'axis'
+            },
+            xAxis : [
+                {
+                    type : 'category',
+                    boundaryGap : false,
+                    data : ['Mon','Thu','Wed','Tue','Fri','Sat','Sun']
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+            series : [
+                {
+                    name:'Num',
+                    type:'line',
+                    areaStyle: {normal: {}},
+                    data:[820, 932, 901, 934, 1290, 1330, 1320]
+                }
+            ]
+        };
+
+    }
 };
--- src/views/pie/index.js
+++ src/views/pie/index.js
@@ -1,3 +1,52 @@
 module.exports = {
-    template: require('./template.html')
+    template: require('./template.html'),
+    data: function () {
+        return {
+            pieChartOption: {}
+        }
+    },
+    created: function () {
+        var vm = this;
+
+        vm.pieChartOption = {
+            tooltip : {},
+            series : [
+                {
+                    name:'Num',
+                    type:'pie',
+                    radius : '65%',
+                    center: ['50%', '50%'],
+                    data:[
+                        {value:335, name:'A'},
+                        {value:310, name:'B'},
+                        {value:274, name:'C'},
+                        {value:235, name:'D'},
+                        {value:400, name:'E'}
+                    ].sort(function (a, b) { return a.value - b.value}),
+                    roseType: 'angle',
+                    label: {
+                        normal: {
+                            textStyle: {
+                                color: '#555'
+                            }
+                        }
+                    },
+                    labelLine: {
+                        normal: {
+                            lineStyle: {
+                                color: '#555'
+                            }
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            color: '#c23531',
+                            shadowBlur: 50,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+    }
 };
